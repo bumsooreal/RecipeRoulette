2d0efd72888db0bd54fa90f40ce7c4b345d17613
Paarin Rastogi <paarin.rastogi@berkeley.edu>
2023-04-29 00:43:07 -0700

    Fixed Promise Object Return, Added CSS to display picture and name

--- src/GetData.js
+++ src/GetData.js
@@ -1,4 +1,4 @@
-let bareURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=568643a5&app_key=3d4a367835cf13d3a4c15f1d9c02ae80&random";
+let bareURL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=568643a5&app_key=3d4a367835cf13d3a4c15f1d9c02ae80&random=true";
 
 async function apiCall(url) {
   let response = await fetch(url);
@@ -13,12 +13,14 @@ async function getData(properties) {
     const value = properties[key];
     formattedUrl += `&${key}=${value}`
   }
-  console.log(formattedUrl);
+  // console.log(formattedUrl);
   
-  //.then((data) => {updateData(data)}).catch((error) => {console.log(error)})
-  let waiting = await apiCall(formattedUrl); 
-  console.log(waiting);
-  return waiting;
+  // //.then((data) => {updateData(data)}).catch((error) => {console.log(error)})
+  // let waiting = await apiCall(formattedUrl); 
+  // console.log(waiting);
+  
+  
+  return apiCall(formattedUrl);
 }
 
 export default getData;
---
+++ src/Routes/Results.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom'
+import getData from '../GetData.js'
+import './Results.css'
+/*
+	Results page after clicking generate
+
+	props should return list of food
+*/
+function Results(props) {
+	const location = useLocation();
+	const properties = location.state;
+	const[data, setData] = useState(null)
+	const [topHits, setTopHits] = useState(null); //tophits is an array of atmost 3 options to choose from 
+	
+
+	useEffect(() => {
+		const fetchAndSet = async () => {
+			const response = await getData(properties);
+
+			let options = [];
+			let i = 0;
+			
+			while (i <= 2 && Boolean(response['hits'][i]) ) {
+				options[i] = response['hits'][i];
+				i++;
+			}
+
+			setData(response);
+			setTopHits(options);
+		}
+		fetchAndSet();
+	}, [properties]);
+	
+	
+	
+//need to find a way to put each set of data into a div and have it format such that if there is 3 2 or 1 piece of data then itll still work
+	console.log(topHits)
+
+
+	return (
+		<body>
+
+		<div className="Results">
+			here are ur results
+		
+		<div className='OptionContainer'>	
+		{topHits ?
+			topHits.map((hit) => {
+				return <div className="Option"> 
+							<img className='Image' src={hit['recipe']['image']} alt= 'Recipe Image' />
+							
+							<div className='Caption'> { hit['recipe']['label'].replace("Recipe", '').replace('!',"").replace("recpies",'')} </div>
+						
+						</div>
+			})
+			:
+			<div>Loading...</div>
+		}
+
+		</div>
+
+		<div>
+                <Link to="/foodprofile">
+                    <button>
+						<div>
+							TO FOOD PROFILE
+						</div>
+
+					</button>
+                </Link>
+            </div>
+		</div>
+
+		</body>
+	);
+}
+
+
+export default Results
